e191fbc2040e26f0e97b327bffcadcf49f46cf49
Bill C. <106033471+bi1cor0@users.noreply.github.com>
2024-06-04 16:07:26 -0700

    Updated the DAPI reference link.

--- src/index-variables.mjs
+++ src/index-variables.mjs
@@ -1,4 +1,4 @@
 export const subValue = document.getElementById("subValue");
 export const form = document.getElementById("mainForm")
-export const nameDisplay = document.createElement("h1");
+export const nameDisplay = document.createElement("h2");
 export const monImg = document.createElement(`img`)
